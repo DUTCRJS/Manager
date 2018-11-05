f0ecb9800132cbd5e390578afaa7a768ac244572
codingyunhao <li_yunhao@outlook.com>
2018-11-05 13:10:37 +0800

    通知模块

--- server/Api/studentApi.js
+++ server/Api/studentApi.js
@@ -219,23 +219,27 @@ exports.studentSearchInfo = function (req,callback) {
 };
 
 exports.kindInfo = function (req,callback) {
+    // console.log("11111");
     var kind = req.query.type;
+    console.log("kind " + kind);
     var sql='';
-    if(kind === "10" || kind === "20"||kind === "30"){
+    if(kind === "10" || kind === "20" || kind === "30"){
+        // console.log("222");
         sql =
-            'select distinct info.infoId,info.title,info.startTime,info.endTime,infokind.kind\n' +
+            'select distinct info.infoId,info.title,info.pubTime,info.startTime,info.endTime\n' +
             'from info join infokind on infokind.infoId = info.infoId \n' +
-            'where infokind.kind like "' + str.substr(0, 1) +'%"';
+            'where infokind.kind like "' + kind.substr(0, 1) +'%"';
     }else {
 
      sql =
-        'select distinct info.infoId,info.title,info.startTime,info.endTime,infokind.kind\n' +
+        'select distinct info.infoId,info.title,info.pubTime,info.startTime,info.endTime\n' +
         'from info join infokind on infokind.infoId = info.infoId \n' +
-        'where infokind.kind = "'+kind+'"';
+        'where infokind.kind = "'+ kind +'"';
     }
+    console.log("sql " + sql);
     manager.sequelize.query(sql).then(function (value) {
         value = value[0];
-        console.log("小类的title " + value);
+        console.log("小类的title " + JSON.stringify(value));
         callback(value);
 
     });
